bb7e96b28ab385ad5389e5d0c9b4598a57d7b2e3
fatemeh_bkhshian <flowerfbf18@gmail.com>
2025-08-20 14:36:17 +0330

    packages & reportsJSON

---
+++ app/app.js
@@ -0,0 +1,38 @@
+const fs = require("fs");
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const port = 3000;
+app.use(cors({
+  origin: ["http://localhost:5173"]
+}));
+
+const users = JSON.parse(fs.readFileSync("modules/users/userlist.json"));
+const reports = JSON.parse(fs.readFileSync("modules/reports/reportlist.json"));
+
+app.get("/api/users", (req, res) => {
+
+  res.status(200).json({
+    status: "success",
+    results: users.length,
+    data: {
+      users,
+    },
+  });
+});
+
+app.get("/api/reports", (req, res) => {
+
+  res.status(200).json({
+    status: "success",
+    results: reports.length,
+    data: {
+      reports,
+    },
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
